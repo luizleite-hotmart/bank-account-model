317be7a61caabf5f2ca70b61db7d1b5957236e3c
luizleite-hotmart <luiz.leite@hotmart.com>
2020-03-13 07:12:11 -0300

    create controller

---
+++ src/main/java/org/luizleiteoliveira/rest/ModelController.java
@@ -0,0 +1,18 @@
+package org.luizleiteoliveira.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/models")
+public class ModelController {
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getModelFromCountry() {
+        return Response.ok().build();
+    }
+}
